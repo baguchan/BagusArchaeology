0397ff50aeb13e816810746b14c44d43618a41dc
baguchan <shutux500@gmail.com>
2023-09-16 19:21:47 +0900

    if power is negative. prevent freeze

--- src/main/java/baguchan/bagus_archaeology/element/FreezeElement.java
+++ src/main/java/baguchan/bagus_archaeology/element/FreezeElement.java
@@ -1,6 +1,7 @@
 package baguchan.bagus_archaeology.element;
 
 import baguchan.bagus_archaeology.registry.ModDamageType;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
@@ -18,25 +19,36 @@ public class FreezeElement extends AlchemyElement {
     public void entityAttack(Mob entity, Entity target, Item item, float power) {
         super.entityAttack(entity, target, item, power);
         if (target instanceof LivingEntity living) {
-            living.setTicksFrozen((int) (living.getTicksFrozen() + 20 + power * 20));
-            living.hurt(entity.damageSources().source(ModDamageType.FREEZE, entity), power * 0.1F);
+            if (power < 0) {
+                living.setTicksFrozen((int) (living.getTicksFrozen() + 20 + power * 20));
+                living.hurt(entity.damageSources().source(ModDamageType.FREEZE, entity), power * 0.1F);
+            } else {
+                living.setTicksFrozen(Mth.clamp((int) (living.getTicksFrozen() - power * 20), 0, living.getTicksFrozen()));
+            }
         }
     }
 
     @Override
     public void projectileHit(Projectile projectile, HitResult hitResult, Item item, float power) {
         if (hitResult instanceof EntityHitResult entityHitResult && entityHitResult.getEntity() instanceof LivingEntity living) {
-            living.setTicksFrozen((int) (living.getTicksFrozen() + 20 + power * 10));
-            living.hurt(projectile.damageSources().source(ModDamageType.FREEZE, projectile, projectile.getOwner()), power * 0.1F);
+            if (power < 0) {
+                living.setTicksFrozen((int) (living.getTicksFrozen() + 20 + power * 20));
+                living.hurt(projectile.damageSources().source(ModDamageType.FREEZE, projectile, projectile.getOwner()), power * 0.1F);
+            } else {
+                living.setTicksFrozen(Mth.clamp((int) (living.getTicksFrozen() - power * 20), 0, living.getTicksFrozen()));
+            }
         }
     }
 
     @Override
     public void active(Entity entity, Item item, float power) {
         if (entity instanceof LivingEntity living) {
-            living.setTicksFrozen((int) (living.getTicksFrozen() + 20 + power * 20));
-            living.hurt(entity.damageSources().source(ModDamageType.FREEZE, entity), power * 0.1F);
-
+            if (power < 0) {
+                living.setTicksFrozen((int) (living.getTicksFrozen() + 20 + power * 20));
+                living.hurt(entity.damageSources().source(ModDamageType.FREEZE, entity), power * 0.1F);
+            } else {
+                living.setTicksFrozen(Mth.clamp((int) (living.getTicksFrozen() - power * 20), 0, living.getTicksFrozen()));
+            }
         }
     }
 
