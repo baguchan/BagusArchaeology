8a81a7001251e3f7dfab17704ebf010dac6010da
baguchan <shutux500@gmail.com>
2023-09-18 17:43:33 +0900

    1.20.1-0.7.0

--- src/main/java/baguchan/bagus_archaeology/registry/ModBlocks.java
+++ src/main/java/baguchan/bagus_archaeology/registry/ModBlocks.java
@@ -10,7 +10,6 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.WallSkullBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.registries.DeferredRegister;
@@ -30,11 +29,11 @@ public class ModBlocks {
     public static final RegistryObject<WallSkullBlock> PIGMAN_SKULL_WALL = noItemRegister("pigman_skull_wall", () -> new ModWallSkullBlock(PIGMAN_SKULL_TYPE, BlockBehaviour.Properties.of().strength(1F).sound(SoundType.BONE_BLOCK)));
     public static final RegistryObject<BrushableSoulSandBlock> SUSPICIOUS_SOUL_SAND = register("suspicious_soul_sand", () -> new BrushableSoulSandBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BROWN).strength(0.35F).instrument(NoteBlockInstrument.COW_BELL).speedFactor(0.4F).sound(SoundType.SUSPICIOUS_SAND)));
     public static final RegistryObject<BrushableSoulSoilBlock> SUSPICIOUS_SOUL_SOIL = register("suspicious_soul_soil", () -> new BrushableSoulSoilBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BROWN).strength(0.35F).sound(SoundType.SUSPICIOUS_SAND)));
-    public static final RegistryObject<AlchemyCauldronBlock> ALCHEMY_CAULDRON = noItemRegister("alchemy_cauldron", () -> new AlchemyCauldronBlock(BlockBehaviour.Properties.of().strength(1F, 5F).sound(SoundType.METAL)));
+    public static final RegistryObject<AlchemyCauldronBlock> ALCHEMY_CAULDRON = noItemRegister("alchemy_cauldron", () -> new AlchemyCauldronBlock(BlockBehaviour.Properties.of().strength(1F, 5F).lightLevel(litBlockEmission(10)).sound(SoundType.METAL)));
 
     private static ToIntFunction<BlockState> litBlockEmission(int p_50760_) {
         return (p_50763_) -> {
-            return p_50763_.getValue(BlockStateProperties.LIT) ? p_50760_ : 0;
+            return p_50763_.getValue(AlchemyCauldronBlock.HAS_WATER) ? p_50760_ : 0;
         };
     }
 
